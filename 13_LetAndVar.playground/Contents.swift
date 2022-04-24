c9df220f632017f33bfc15030d8ca5d1e9a854cb
Yuya Yamamura <yuya.yamamura@best-rise.co.jp>
2022-04-25 07:09:09 +0900

    first commit

---
+++ 13_LetAndVar.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let a = 123
+//a = 406
+
+var b = 123
+b = 456
---
+++ 18_Branch.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+let a = true
+
+if a {
+    print("Hello!")
+} else {
+    print("Hi!")
+}
+
+let time = 21
+
+if time < 12 {
+    print("Good morning!")
+} else if time < 18 {
+    print("Good afternoon!")
+} else {
+    print("Good night!")
+}
